70e8727171280a6755853ec6cc9f4327a794524d
AyyanNadeem01 <ayyan137a@gmail.com>
2025-10-12 19:30:43 +0500

    myarray done

---
+++ basic2/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros:string[]=[]
+//by default never,if empty error also error, mention type
+superHeros.push("spiderman")
+
+// const heroPower:number[]=[]
+// heroPower.push(2)
+
+const heroPower:Array<number>=[]
+heroPower.push(2)
+
+type User={
+    name:string
+    isActive:boolean
+}
+
+const allUsers:User[]=[]
+
+allUsers.push({name:"",isActive:true})
+
+const MLModels:number[][]=[[255,255,255],[]]
